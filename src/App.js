a3e1327a00defe8dfb2c068a050f39781ef8bb9b
Pedro Cobos <pedro.cobos@live.com>
2021-01-11 19:08:13 -0600

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Hello Pedro</h1>
     </div>
   );
 }
