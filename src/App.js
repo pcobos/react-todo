786c4ed9b91cc2502b8bb5f5ff8909603b2a05f8
Pedro Cobos <pedro.cobos@live.com>
2021-01-11 19:29:51 -0600

    feat: add form JSX to Form component and exported it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Pedro</h1>
+      <header>
+        <h1>Pedro's Todo list</h1>
+      </header>
+      <Form />
     </div>
   );
 }
