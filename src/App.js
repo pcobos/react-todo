f463f635c86d2110d8d3452c9b1d06e21654368c
Pedro Cobos <pedro.cobos@live.com>
2021-01-16 21:50:35 -0600

    feat: add filterHandler method to pass the todos to filteredTodos state based on their status

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  // state variables
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
+
+  const filterHandler = () => {
+    switch(status) {
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  }
 
   return (
     <div className="App">
